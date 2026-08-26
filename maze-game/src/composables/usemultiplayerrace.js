4540b0e978df65e06f33bca2738d643720dd9ae1
Lakshmi.Sharma <lakshmi.sharma@sonartechnologies.com>
2026-08-26 13:58:43 +0530

    Test

--- maze-game/src/composables/usemultiplayerrace.js
+++ maze-game/src/composables/usemultiplayerrace.js
@@ -3,6 +3,7 @@ import * as signalR from '@microsoft/signalr'
 
 
 const HUB_URL = import.meta.env.VITE_APP_SIGNALR_HUBURL
+console.log(import.meta.env.VITE_APP_SIGNALR_HUBURL)
 
 export function useMultiplayerRace() {
   const connection = ref(null)
